b3931382a306ab381ee536b1a784e6681c582721
Ashu <ashu@gmail.com>
2021-10-17 17:59:25 +0530

    Ready to deploy

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const cheerio = require("cheerio");
 async function requestTo(query) {
     try {
         const flipkartRes = await got(`https://www.flipkart.com/search?q=${query}`);
-        let fdata = flipkartData(flipkartRes.body);
+        let Flipkart = flipkartData(flipkartRes.body);
         const amazonRes = await got(`https://www.amazon.in/s?k=${query}`);
-        let adata = amazonData(amazonRes.body);
-        return { fdata, adata };
+        let Amazon = amazonData(amazonRes.body);
+
+        return { Flipkart, Amazon };
 
     } catch (error) {
         console.log(error);
@@ -24,19 +25,19 @@ function amazonData(html) {
     let data = {};
 
     let $ = cheerio.load(html);
-    
+
     let aTag = $('div[class="sg-col-inner"] a[class="a-link-normal s-no-outline"]');
 
     let nameTag = $('div[class="sg-col-inner"] a[class="a-link-normal a-text-normal"] span[class="a-size-medium a-color-base a-text-normal"]');
 
     let imgTag = $('div[class="sg-col-inner"] a[class="a-link-normal s-no-outline"] img[class="s-image"]');
-    
+
     let priceTag = $('div[class="sg-col-inner"] a[class="a-size-base a-link-normal a-text-normal"] span[class="a-price-whole"]');
 
+    let length = aTag.length > 8 ? 8 : aTag.length;
 
-
-    for (let i = 0; i < aTag.length; i++) {
-        data[`product${i}`] = { productName: $(nameTag[i]).text(), productLink: `https://www.amazon.in${$(aTag[i]).attr('href')}`, productImg: $(imgTag[i]).attr('src'), productPrice: $(priceTag[i]).text() }
+    for (let i = 0; i < length; i++) {
+        data[`product${i}`] = { brand: "Amazon", productName: $(nameTag[i]).text(), productLink: `https://www.amazon.in${$(aTag[i]).attr('href')}`, productImg: $(imgTag[i]).attr('src'), productPrice: $(priceTag[i]).text() }
     }
 
     return data;
@@ -56,8 +57,9 @@ function flipkartData(html) {
     let priceTag = $("._1fQZEK ._30jeq3._1_WHN1")
 
 
+
     for (let i = 0; i < aTag.length; i++) {
-        data[`product${i}`] = { productName: $(nameTag[i]).text(), productLink: `https://www.flipkart.com${$(aTag[i]).attr('href')}`, productImg: $(imgTag[i]).attr('src'), productPrice: $(priceTag[i]).text() }
+        data[`product${i}`] = { brand: "Flipkart", productName: $(nameTag[i]).text(), productLink: `https://www.flipkart.com${$(aTag[i]).attr('href')}`, productImg: $(imgTag[i]).attr('src'), productPrice: $(priceTag[i]).text() }
     }
 
     return data;
@@ -69,3 +71,42 @@ function flipkartData(html) {
 module.exports = requestTo;
 
 // requestTo();
+
+
+
+
+
+
+
+
+
+
+
+
+// <% for (let key in result) {
+//     if (result[key] !== " ") {%>
+//                 <div class="product_brand">
+//                     <h1>
+//                         Brand
+//                     </h1>
+//                 </div>
+//                 <div class="products_grid">
+//                     <%for(let subKey in result[key]){ if(result[key][subKey]!=="" ){%>
+//                         <a href="<%=result[key][subKey]['productLink']%>" class="product">
+
+//                             <img src="<%=result[key][subKey]['productImg']%>">
+//                             <h4>
+//                                 <%=result[key][subKey]["productName"]%>
+//                             </h4>
+//                             <h3 class="price">Price: <span>
+//                                     <%=result[key][subKey]["productPrice"]%>
+//                                 </span> </h3>
+
+//                         </a>
+//                         <%}else{%>
+//                             <h1>No Result Found</h1>
+//                             <%}}%>
+//                 </div>
+
+//                 <%}
+// }%>
--- public/js/products.js
+++ public/js/products.js
@@ -4,3 +4,9 @@ const hamburger_navMenu = document.querySelector(".hamburger_navMenu");
 hamburger.addEventListener("click", () => {
     hamburger_navMenu.classList.toggle("show");
 });
+
+
+
+// for (const key in { a: "1", b, "2"}) {
+//     console.log(key);
+// }
